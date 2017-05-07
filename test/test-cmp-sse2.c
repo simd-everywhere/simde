3f8f85b15274cd0c1ce0db2eefffe64178b85283
Evan Nemerson <evan@nemerson.com>
2017-05-07 15:10:15 -0700

    sse2: add comparison tests for subtraction functions

--- test/test-cmp-sse2.c
+++ test/test-cmp-sse2.c
@@ -45,8 +45,16 @@ define_test_cmp_f64_VT_2xVT(_m128d, mm_and_pd)
 define_test_cmp_f64_VT_2xVT(_m128d, mm_andnot_pd)
 define_test_cmp_VT_2xVT(_m128i, mm_avg_epu8, u8)
 define_test_cmp_VT_2xVT(_m128i, mm_avg_epu16, u16)
-
-/* define_test_cmp_VT_2xVT(_m128d, mm_adds_epu8, u8) */
+define_test_cmp_VT_2xVT(_m128i, mm_sub_epi8, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_sub_epi16, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_sub_epi32, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_sub_epi64, i8)
+define_test_cmp_f64_VT_2xVT(_m128d, mm_sub_pd)
+define_test_cmp_f64_VT_2xVT(_m128d, mm_sub_sd)
+define_test_cmp_VT_2xVT(_m128i, mm_subs_epi8, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_subs_epi16, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_subs_epu8, i8)
+define_test_cmp_VT_2xVT(_m128i, mm_subs_epu16, i8)
 
 static MunitTest test_suite_tests[] = {
   { (char*) "/mm_add_epi8",       test_simde_mm_add_epi8,       NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
@@ -63,6 +71,16 @@ static MunitTest test_suite_tests[] = {
   { (char*) "/mm_andnot_pd",      test_simde_mm_andnot_pd,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "/mm_avg_epu8",       test_simde_mm_avg_epu8,       NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "/mm_avg_epu16",      test_simde_mm_avg_epu16,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_epi8",       test_simde_mm_sub_epi8,       NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_epi16",      test_simde_mm_sub_epi16,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_epi32",      test_simde_mm_sub_epi32,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_epi64",      test_simde_mm_sub_epi64,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_pd",         test_simde_mm_sub_pd,         NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_sub_sd",         test_simde_mm_sub_sd,         NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_subs_epi8",      test_simde_mm_subs_epi8,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_subs_epi16",     test_simde_mm_subs_epi16,     NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_subs_epu8",      test_simde_mm_subs_epu8,      NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "/mm_subs_epu16",     test_simde_mm_subs_epu16,     NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
