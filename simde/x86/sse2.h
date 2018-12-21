f4addf0aae8e0efc0ac6c692fa4f6a7a0b078164
marmeladema <xademax@gmail.com>
2018-12-21 12:40:18 +0000

    sse2: add NEON implementation of _mm_set1_epi64x

--- simde/x86/sse2.h
+++ simde/x86/sse2.h
@@ -2583,6 +2583,8 @@ simde_mm_set1_epi64x (int64_t a) {
 
 #if defined(SIMDE_SSE2_NATIVE)
   r.n = _mm_set1_epi64x(a);
+#elif defined(SIMDE_SSE2_NEON)
+  r.neon_i64 = vmovq_n_s64(a);
 #else
   SIMDE__VECTORIZE
   for (size_t i = 0 ; i < (sizeof(r.i64) / sizeof(r.i64[0])) ; i++) {
