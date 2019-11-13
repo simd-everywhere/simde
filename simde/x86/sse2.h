simd-everywhere/simde

commit 941c09756365122498b897cdebc0e5e209336057

Remove SIMDE__REALLY_GCC; that functionality has moved to Hedley

simde/x86/sse2.h
@@ -1571,7 +1571,7 @@ int32_t
 simde_mm_extract_epi16 (simde__m128i a, const int imm8) {
   return a.u16[imm8 & 7];
 }
-#if defined(SIMDE_SSE2_NATIVE) && (!defined(SIMDE__REALLY_GCC) || HEDLEY_GCC_VERSION_CHECK(4,6,0))
+#if defined(SIMDE_SSE2_NATIVE) && (!defined(HEDLEY_GCC_VERSION) || HEDLEY_GCC_VERSION_CHECK(4,6,0))
 #  define simde_mm_extract_epi16(a, imm8) _mm_extract_epi16(a.n, imm8)
 #elif defined(SIMDE_SSE2_NEON)
 #  define simde_mm_extract_epi16(a, imm8) (vgetq_lane_s16((a).neon_i16, (imm8)) & ((int32_t) UINT32_C(0x0000ffff)))
@@ -3426,12 +3426,12 @@ SIMDE__FUNCTION_ATTRIBUTES
 void
 simde_mm_stream_si64 (int64_t* mem_addr, int64_t a) {
 #if defined(SIMDE_SSE2_NATIVE) && defined(SIMDE_ARCH_AMD64)
-  #if defined(SIMDE__REALLY_GCC) && !HEDLEY_GCC_VERSION_CHECK(4,8,0)
-    *mem_addr = a;
-  #elif defined(__GNUC__)
+  #if defined(HEDLEY_GCC_VERSION)
     _mm_stream_si64((long long*) mem_addr, a);
-  #else
+  #elif !defined(HEDLEY_PGI_VERSION)
     _mm_stream_si64(mem_addr, a);
+  #else
+    *mem_addr = a;
   #endif
 #else
   *mem_addr = a;

simde/simde-common.h
@@ -221,20 +221,6 @@ HEDLEY_STATIC_ASSERT(sizeof(simde_float64) == 8, "Unable to find 64-bit floating
 #  define SIMDE_ACCURACY_ITERS 2
 #endif
 
-/* This will probably move into Hedley at some point, but I'd like to
-   more thoroughly check for other compilers which define __GNUC__
-   first. */
-#if defined(SIMDE__REALLY_GCC)
-#  undef SIMDE__REALLY_GCC
-#endif
-#if !defined(__GNUC__) || \
-  defined(__clang__) || \
-  defined(__INTEL_COMPILER)
-#define SIMDE__REALLY_GCC 0
-#else
-#define SIMDE__REALLY_GCC 1
-#endif
-
 #if defined(SIMDE__ASSUME_ALIGNED)
 #  undef SIMDE__ASSUME_ALIGNED
 #endif
@@ -259,18 +245,16 @@ HEDLEY_STATIC_ASSERT(sizeof(simde_float64) == 8, "Unable to find 64-bit floating
    start only defining them for problematic compiler versions. */
 
 #if !defined(SIMDE_IGNORE_COMPILER_BUGS)
-#  if SIMDE__REALLY_GCC
-#    if !HEDLEY_GCC_VERSION_CHECK(4,9,0)
-#      define SIMDE_BUG_GCC_REV_208793
-#    endif
-#    if !HEDLEY_GCC_VERSION_CHECK(5,0,0)
-#      define SIMDE_BUG_GCC_BAD_MM_SRA_EPI32 /* TODO: find relevant bug or commit */
-#    endif
-#    if !HEDLEY_GCC_VERSION_CHECK(4,6,0)
-#      define SIMDE_BUG_GCC_BAD_MM_EXTRACT_EPI8 /* TODO: find relevant bug or commit */
-#    endif
+#  if !HEDLEY_GCC_VERSION_CHECK(4,9,0)
+#    define SIMDE_BUG_GCC_REV_208793
 #  endif
-#  if defined(__EMSCRIPTEN__)
+#  if !HEDLEY_GCC_VERSION_CHECK(5,0,0)
+#    define SIMDE_BUG_GCC_BAD_MM_SRA_EPI32 /* TODO: find relevant bug or commit */
+#  endif
+#  if !HEDLEY_GCC_VERSION_CHECK(4,6,0)
+#    define SIMDE_BUG_GCC_BAD_MM_EXTRACT_EPI8 /* TODO: find relevant bug or commit */
+#  endif
+#  if defined(HEDLEY_EMSCRIPTEN_VERSION)
 #    define SIMDE_BUG_EMSCRIPTEN_MISSING_IMPL /* Placeholder for (as yet) unfiled issues. */
 #    define SIMDE_BUG_EMSCRIPTEN_5242
 #  endif

commit dd4986c912f6fc1482395ea4d3aeb4081b7f06a3

sse2: fix _mm_stream_si64 GCC < 5.3
I broke it in 941c09756365122498b897cdebc0e5e209336057.  Oops.

simde/x86/sse2.h
@@ -3426,9 +3426,9 @@ SIMDE__FUNCTION_ATTRIBUTES
 void
 simde_mm_stream_si64 (int64_t* mem_addr, int64_t a) {
 #if defined(SIMDE_SSE2_NATIVE) && defined(SIMDE_ARCH_AMD64)
-  #if defined(HEDLEY_GCC_VERSION)
+  #if HEDLEY_GCC_VERSION_CHECK(5,3,0) // https://github.com/gcc-mirror/gcc/commit/821b85a2a0869b5128bd67e192a5a97748cbd90d
     _mm_stream_si64((long long*) mem_addr, a);
-  #elif !defined(HEDLEY_PGI_VERSION)
+  #elif !defined(HEDLEY_PGI_VERSION) && !defined(HEDLEY_GCC_VERSION)
     _mm_stream_si64(mem_addr, a);
   #else
     *mem_addr = a;