59682f5a43efe7ae4f52e94e6a75d9076dcc7519
Hidayat Khan <huk2209@gmail.com>
2020-03-08 00:05:09 +0530

    sse.h: wasm implementation for mm_mul_ps added

--- simde/x86/sse.h
+++ simde/x86/sse.h
@@ -2345,6 +2345,8 @@ simde_mm_mul_ps (simde__m128 a, simde__m128 b) {
 
 #if defined(SIMDE_SSE_NEON)
   r_.neon_f32 = vmulq_f32(a_.neon_f32, b_.neon_f32);
+#elif defined(SIMDE_SSE_WASM_SIMD128)
+  r_.wasm_v128 = wasm_f32x4_mul(a_.wasm_v128, b_.wasm_v128);
 #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
   r_.f32 = a_.f32 * b_.f32;
 #else
