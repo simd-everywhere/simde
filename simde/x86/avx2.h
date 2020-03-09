a520b6455bca130f954c75988130bd1e0692e897
Himanshi Mathur <himanshi18037@iiitd.ac.in>
2020-03-09 12:46:13 +0530

    Added implmentation of _mm256_srli_epi32 and _mm256_max_epu16 in simde/x86/avx2.h file and test case in test/x86/avx2.c file

--- simde/x86/avx2.h
+++ simde/x86/avx2.h
@@ -893,6 +893,34 @@ simde_mm256_max_epu8 (simde__m256i a, simde__m256i b) {
 
 SIMDE__FUNCTION_ATTRIBUTES
 simde__m256i
+simde_mm256_max_epu16 (simde__m256i a, simde__m256i b) {
+#if defined(SIMDE_AVX2_NATIVE)
+  return _mm256_max_epu16(a, b);
+#else
+  simde__m256i_private
+    r_,
+    a_ = simde__m256i_to_private(a),
+    b_ = simde__m256i_to_private(b);
+
+#if defined(SIMDE_ARCH_X86_SSE2) || defined(SIMDE_SSE2_NEON)
+  r_.m128i[0] = simde_mm_max_epu16(a_.m128i[0], b_.m128i[0]);
+  r_.m128i[1] = simde_mm_max_epu16(a_.m128i[1], b_.m128i[1]);
+#else
+  SIMDE__VECTORIZE
+  for (size_t i = 0 ; i < (sizeof(r_.u16) / sizeof(r_.u16[0])) ; i++) {
+    r_.u16[i] = (a_.u16[i] > b_.u16[i]) ? a_.u16[i] : b_.u16[i];
+  }
+#endif
+
+  return simde__m256i_from_private(r_);
+#endif
+}
+#if defined(SIMDE_AVX2_ENABLE_NATIVE_ALIASES)
+#  define _mm256_max_epu16(a, b) simde_mm256_max_epu16(a, b)
+#endif
+
+SIMDE__FUNCTION_ATTRIBUTES
+simde__m256i
 simde_mm256_max_epi16 (simde__m256i a, simde__m256i b) {
 #if defined(SIMDE_AVX2_NATIVE)
   return _mm256_max_epi16(a, b);
