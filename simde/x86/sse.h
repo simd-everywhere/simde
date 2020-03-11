93a96997f7bd7ee0494213f9590acc81de61a5e7
Hidayat Khan <huk2209@gmail.com>
2020-03-11 16:56:26 +0530

    sse.h: added implementation for mm_andnot_ps

--- simde/x86/sse.h
+++ simde/x86/sse.h
@@ -326,6 +326,8 @@ simde_mm_andnot_ps (simde__m128 a, simde__m128 b) {
 
 #if defined(SIMDE_SSE_NEON)
   r_.neon_i32 = vbicq_s32(b_.neon_i32, a_.neon_i32);
+#elif defined(SIMDE_SSE_WASM_SIMD128)
+  r_.wasm_v128 = wasm_v128_andnot(b_.wasm_v128, a_.wasm_v128);
 #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
   r_.i32 = ~a_.i32 & b_.i32;
 #else
