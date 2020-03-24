62fa607983a73f58fd64c8b7ef6ba3e20e22b959
Himanshi Mathur <himanshi18037@iiitd.ac.in>
2020-03-25 04:09:24 +0530

    avx512bw: add simde_mm512_adds_epu{8,16}

--- simde/x86/avx512bw.h
+++ simde/x86/avx512bw.h
@@ -106,6 +106,52 @@ simde_mm512_add_epi16 (simde__m512i a, simde__m512i b) {
 #  define _mm512_add_epi16(a, b) simde_mm512_add_epi16(a, b)
 #endif
 
+SIMDE__FUNCTION_ATTRIBUTES
+simde__m512i
+simde_mm512_adds_epu8 (simde__m512i a, simde__m512i b) {
+#if defined(SIMDE_AVX512BW_NATIVE)
+  return _mm512_adds_epu8(a, b);
+#else
+  simde__m512i_private
+    r_,
+    a_ = simde__m512i_to_private(a),
+    b_ = simde__m512i_to_private(b);
+
+  SIMDE__VECTORIZE
+  for (size_t i = 0 ; i < (sizeof(r_.m128i) / sizeof(r_.m128i[0])) ; i++) {
+    r_.m128i[i] = simde_mm_adds_epu8(a_.m128i[i], b_.m128i[i]);
+  }
+
+  return simde__m512i_from_private(r_);
+#endif
+}
+#if defined(SIMDE_AVX512BW_ENABLE_NATIVE_ALIASES)
+#  define _mm512_adds_epu8(a, b) simde_mm512_adds_epu8(a, b)
+#endif
+
+SIMDE__FUNCTION_ATTRIBUTES
+simde__m512i
+simde_mm512_adds_epu16 (simde__m512i a, simde__m512i b) {
+#if defined(SIMDE_AVX512BW_NATIVE)
+  return _mm512_adds_epu16(a, b);
+#else
+  simde__m512i_private
+    r_,
+    a_ = simde__m512i_to_private(a),
+    b_ = simde__m512i_to_private(b);
+
+  SIMDE__VECTORIZE
+  for (size_t i = 0 ; i < (sizeof(r_.m128i) / sizeof(r_.m128i[0])) ; i++) {
+    r_.m128i[i] = simde_mm_adds_epu16(a_.m128i[i], b_.m128i[i]);
+  }
+
+  return simde__m512i_from_private(r_);
+#endif
+}
+#if defined(SIMDE_AVX512BW_ENABLE_NATIVE_ALIASES)
+#  define _mm512_adds_epu16(a, b) simde_mm512_adds_epu16(a, b)
+#endif
+
 SIMDE__END_DECLS
 
 HEDLEY_DIAGNOSTIC_POP
