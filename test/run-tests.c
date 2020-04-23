8fdc596061c740477649b903a6f7e1fb55fc3062
Evan Nemerson <evan@nemerson.com>
2020-04-22 20:37:38 -0700

    tests: don't run NEON tests
    The NEON code is pretty broken, which is why we're not installing the
    headers.  These tests will be re-enabled once the NEON stuff is
    re-written.
    
    Fixes #202

--- test/run-tests.c
+++ test/run-tests.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[HEDLEY_ARRAY_PARAM(argc + 1)]) {
   static MunitSuite suite = { "", NULL, NULL, 1, MUNIT_SUITE_OPTION_NONE };
   MunitSuite suites[] = {
     *simde_tests_x86_get_suite(),
-    *simde_tests_arm_get_suite(),
+    /* *simde_tests_arm_get_suite(), */
     { NULL, NULL, NULL, 1, MUNIT_SUITE_OPTION_NONE }
   };
 
