882764e1b61d90305bd239d956d066ec13af52db
Evan Nemerson <evan@nemerson.com>
2020-06-26 15:26:37 -0700

    native-aliases: add NEON native aliases to test

--- test/native-aliases.sh
+++ test/native-aliases.sh
@@ -12,14 +12,17 @@
 #   CFLAGS='-DSIMDE_ENABLE_NATIVE_ALIASES -DSIMDE_NATIVE_ALIASES_TESTING' CXXFLAGS='-DSIMDE_ENABLE_NATIVE_ALIASES -DSIMDE_NATIVE_ALIASES_TESTING' meson ..
 #   ninja
 
-cd "$(dirname "${0}")"
+DIRNAME="$(realpath "$(dirname "${0}")")"
+cd "${DIRNAME}"
 
-if [ ! -e iig.xml ]; then
-  VERSION="$(curl -s 'https://software.intel.com/sites/landingpage/IntrinsicsGuide/' | grep -Po '(?<=intrinsicsguide.min.js\?)([0-9\.]+)')"
-  curl "https://software.intel.com/sites/landingpage/IntrinsicsGuide/files/data-${VERSION}.xml" > iig.xml
-fi
+# x86
 
-PATTERN="$(xmllint --xpath '//intrinsic/@name' iig.xml | grep -Po '(?<=")[^"]+' | grep -Pv '^(_mm256_cvtsi256_si32|_mm512_loadu_epi.+)$' | xargs printf '%s|' | rev | cut -c 2- | rev)"
+./download-iig.sh "${DIRNAME}/iig.xml"
+PATTERN="$(xmllint --xpath '//intrinsic/@name' "${DIRNAME}/iig.xml" | grep -Po '(?<=")[^"]+' | grep -Pv '^(_mm256_cvtsi256_si32|_mm512_loadu_epi.+)$' | xargs printf '%s|' | rev | cut -c 2- | rev)"
 echo "s/([^_])simde(${PATTERN})/\1\2/g" > pattern
-
 ls x86/*.c | xargs -n1 -P$(nproc) sed -i -E -f pattern
+
+
+# NEON
+
+perl -p -i -e 's/([^a-zA-Z0-9_])simde_v/$1v/g' arm/neon/*.{c,h}
