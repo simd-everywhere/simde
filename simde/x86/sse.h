9ad3a57b8058c4a3bd318ff4db390a0e57962743
Hidayat <huk2209@gmail.com>
2020-07-15 17:35:20 -0400

    sse: added NEON aarch64 impl for mm_movemask_ps

--- simde/x86/sse.h
+++ simde/x86/sse.h
@@ -2897,7 +2897,12 @@ simde_mm_movemask_ps (simde__m128 a) {
   int r = 0;
   simde__m128_private a_ = simde__m128_to_private(a);
 
-  #if defined(SIMDE_ARM_NEON_A32V7_NATIVE)
+  #if defined(SIMDE_ARM_NEON_A64V8_NATIVE)
+    static const int32x4_t shift = {-31, -30, -29, -28};
+    static const uint32x4_t highbit = {0x80000000, 0x80000000, 0x80000000,
+                                       0x80000000};
+    return vaddvq_u32(vshlq_u32(vandq_u32(a_.neon_u32, highbit), shift));
+  #elif defined(SIMDE_ARM_NEON_A32V7_NATIVE)
     // Shift out everything but the sign bits with a 32-bit unsigned shift right.
     uint64x2_t high_bits = vreinterpretq_u64_u32(vshrq_n_u32(a_.neon_u32, 31));
     // Merge the two pairs together with a 64-bit unsigned shift right + add.
