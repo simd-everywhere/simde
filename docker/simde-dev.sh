88e23ef7fefeaa8d8c3cb33773397d21d5023a07
Evan Nemerson <evan@nemerson.com>
2020-08-03 10:06:22 -0700

    docker: add Dockerfile for Debian unstable
    Debian testing is broken at the moment since x86_64 has a different
    GCC version that all the other architectures.  To use this, just do
    `./docker/simde-dev.sh unstable`.

--- docker/simde-dev.sh
+++ docker/simde-dev.sh
@@ -8,11 +8,30 @@ if [ "$(basename "${DOCKER}")" = "podman" ]; then
   PODMAN_RELABEL=":z"
 fi
 
-if [ $# = 0 ]; then
-  CMD="/bin/bash"
+if [ "${OSTYPE}" = "linux-gnu" ]; then
+  CAPABILITIES="--cap-add=CAP_SYS_PTRACE";
 else
-  CMD="$@"
+  CAPABILITIES="";
 fi
 
-"${DOCKER}" build -t 'simde-dev' --cap-add=CAP_SYS_PTRACE -f "${DOCKER_DIR}/Dockerfile" "${DOCKER_DIR}/.."
-"${DOCKER}" run -v "$(realpath "${DOCKER_DIR}/..")":/usr/local/src/simde${PODMAN_RELABEL} --cap-add=CAP_SYS_PTRACE --rm -it simde-dev ${CMD}
+IMAGE_NAME="simde-dev"
+
+if [ -z "${1}" ]; then
+  DOCKERFILE="${DOCKER_DIR}/Dockerfile"
+  IMAGE_NAME="simde-testing"
+else
+  if [ -e "$1" ]; then
+    DOCKERFILE="${1}";
+  elif [ -e "${DOCKER_DIR}/Dockerfile-${1}" ]; then
+    DOCKERFILE="${DOCKER_DIR}/Dockerfile-${1}"
+    IMAGE_NAME="simde-${1}"
+  else
+    echo "Unable to find Dockerfile for ${1}" >&2
+    exit 1
+  fi
+fi
+
+echo "Using $(realpath "${DOCKERFILE}")"
+
+"${DOCKER}" build -t "${IMAGE_NAME}" --cap-add=CAP_SYS_PTRACE -f "${DOCKERFILE}" "${DOCKER_DIR}/.."
+"${DOCKER}" run -v "$(realpath "${DOCKER_DIR}/..")":/usr/local/src/simde${PODMAN_RELABEL} ${CAPABILITIES} --rm -it "${IMAGE_NAME}" /bin/bash
