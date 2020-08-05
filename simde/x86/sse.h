c061dd6dba6bd06c85687fcd17a44aa2723b67b4
Evan Nemerson <evan@nemerson.com>
2020-08-04 20:15:39 -0700

    sse: add SIMDE_VECTORIZE to simde_mm_rcp_ps loop

--- simde/x86/sse.h
+++ simde/x86/sse.h
@@ -3079,6 +3079,7 @@ simde_mm_rcp_ps (simde__m128 a) {
       r_.f32 = 1.0f / a_.f32;
     #elif defined(__STDC_IEC_559__)
       /* https://stackoverflow.com/questions/12227126/division-as-multiply-and-lut-fast-float-division-reciprocal/12228234#12228234 */
+      SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.f32) / sizeof(r_.f32[0])) ; i++) {
         int32_t ix;
         simde_float32 fx = a_.f32[i];
