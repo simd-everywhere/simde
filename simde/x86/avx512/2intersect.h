7758105d6513861b979315a6a2e5efc78c3022e6
Ashleigh Newman-Jones <ashnewman-jones@hotmail.co.uk>
2020-10-06 10:30:00 +0100

    avx512vp2intersect review * Remove branch inside loop * Use temp local variable * Remove memset * Fix incorrect #if #else #endif Add 2intersect to declare-suites.h

--- simde/x86/avx512/2intersect.h
+++ simde/x86/avx512/2intersect.h
@@ -39,22 +39,26 @@ void
 simde_mm_2intersect_epi32(simde__m128i a, simde__m128i b, simde__mmask8 *k1, simde__mmask8 *k2) {
   #if defined(SIMDE_X86_AVX512VP2INTERSECT_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     _mm_2intersect_epi32(a, b, k1, k2);
-  #endif
+  #else
     simde__m128i_private
-    a_ = simde__m128i_to_private(a),
-    b_ = simde__m128i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask8));
-    simde_memset(k2, 0, sizeof(simde__mmask8));
+      a_ = simde__m128i_to_private(a),
+      b_ = simde__m128i_to_private(b);
+    simde__mmask8
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i32) / sizeof(a_.i32[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i32) / sizeof(b_.i32[0]) ; j++) {
-        if (a_.i32[i] == b_.i32[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i32[i] == b_.i32[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
+  #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef __mm_2intersect_epi32
@@ -66,22 +70,26 @@ void
 simde_mm_2intersect_epi64(simde__m128i a, simde__m128i b, simde__mmask8 *k1, simde__mmask8 *k2) {
   #if defined(SIMDE_X86_AVX512VP2INTERSECT_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     _mm_2intersect_epi64(a, b, k1, k2);
-  #endif
+  #else
     simde__m128i_private
-    a_ = simde__m128i_to_private(a),
-    b_ = simde__m128i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask8));
-    simde_memset(k2, 0, sizeof(simde__mmask8));
+      a_ = simde__m128i_to_private(a),
+      b_ = simde__m128i_to_private(b);
+    simde__mmask8
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i64) / sizeof(a_.i64[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i64) / sizeof(b_.i64[0]) ; j++) {
-        if (a_.i64[i] == b_.i64[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i64[i] == b_.i64[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
+  #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef __mm_2intersect_epi64
@@ -93,22 +101,26 @@ void
 simde_mm256_2intersect_epi32(simde__m256i a, simde__m256i b, simde__mmask8 *k1, simde__mmask8 *k2) {
   #if defined(SIMDE_X86_AVX512VP2INTERSECT_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     _mm256_2intersect_epi32(a, b, k1, k2);
-  #endif
+  #else
     simde__m256i_private
-    a_ = simde__m256i_to_private(a),
-    b_ = simde__m256i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask8));
-    simde_memset(k2, 0, sizeof(simde__mmask8));
+      a_ = simde__m256i_to_private(a),
+      b_ = simde__m256i_to_private(b);
+    simde__mmask8
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i32) / sizeof(a_.i32[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i32) / sizeof(b_.i32[0]) ; j++) {
-        if (a_.i32[i] == b_.i32[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i32[i] == b_.i32[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
+  #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm256_2intersect_epi32
@@ -120,22 +132,26 @@ void
 simde_mm256_2intersect_epi64(simde__m256i a, simde__m256i b, simde__mmask8 *k1, simde__mmask8 *k2) {
   #if defined(SIMDE_X86_AVX512VP2INTERSECT_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     _mm256_2intersect_epi64(a, b, k1, k2);
-  #endif
+  #else
     simde__m256i_private
-    a_ = simde__m256i_to_private(a),
-    b_ = simde__m256i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask8));
-    simde_memset(k2, 0, sizeof(simde__mmask8));
+      a_ = simde__m256i_to_private(a),
+      b_ = simde__m256i_to_private(b);
+    simde__mmask8
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i64) / sizeof(a_.i64[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i64) / sizeof(b_.i64[0]) ; j++) {
-        if (a_.i64[i] == b_.i64[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i64[i] == b_.i64[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
+  #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm256_2intersect_epi64
@@ -149,20 +165,23 @@ simde_mm512_2intersect_epi32(simde__m512i a, simde__m512i b, simde__mmask16 *k1,
     _mm512_2intersect_epi32(a, b, k1, k2);
   #else
     simde__m512i_private
-    a_ = simde__m512i_to_private(a),
-    b_ = simde__m512i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask16));
-    simde_memset(k2, 0, sizeof(simde__mmask16));
+      a_ = simde__m512i_to_private(a),
+      b_ = simde__m512i_to_private(b);
+    simde__mmask16
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i32) / sizeof(a_.i32[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i32) / sizeof(b_.i32[0]) ; j++) {
-        if (a_.i32[i] == b_.i32[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i32[i] == b_.i32[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
   #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES)
@@ -177,20 +196,23 @@ simde_mm512_2intersect_epi64(simde__m512i a, simde__m512i b, simde__mmask8 *k1, 
     _mm512_2intersect_epi64(a, b, k1, k2);
   #else
     simde__m512i_private
-    a_ = simde__m512i_to_private(a),
-    b_ = simde__m512i_to_private(b);
-    simde_memset(k1, 0, sizeof(simde__mmask8));
-    simde_memset(k2, 0, sizeof(simde__mmask8));
+      a_ = simde__m512i_to_private(a),
+      b_ = simde__m512i_to_private(b);
+    simde__mmask8
+      k1_ = 0,
+      k2_ = 0;
 
     for (size_t i = 0 ; i < sizeof(a_.i64) / sizeof(a_.i64[0]) ; i++) {
       SIMDE_VECTORIZE
       for (size_t j = 0 ; j < sizeof(b_.i64) / sizeof(b_.i64[0]) ; j++) {
-        if (a_.i64[i] == b_.i64[j]) {
-          *k1 |= 1 << i;
-          *k2 |= 1 << j;
-        }
+        const int32_t m = a_.i64[i] == b_.i64[j];
+        k1_ |= m << i;
+        k2_ |= m << j;
       }
     }
+
+    *k1 = k1_;
+    *k2 = k2_;
   #endif
 }
 #if defined(SIMDE_X86_AVX512VP2INTERSECT_ENABLE_NATIVE_ALIASES)
