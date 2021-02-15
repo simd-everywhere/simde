87bb927fa4a3a4db7a2164849de27044e262fcb9
Evan Nemerson <evan@nemerson.com>
2021-02-15 10:38:06 -0500

    download-sde: be more tolerant of changes on Intel's web site

--- test/download-sde.sh
+++ test/download-sde.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 FRAGMENT="$(curl -sL 'https://software.intel.com/content/www/us/en/develop/articles/pre-release-license-agreement-for-intel-software-development-emulator-accept-end-user-license-agreement-and-download.html' | \
-    grep -oP '/content/dam/develop/external/us/en/documents/sde-external-([0-9\.\-]+)-lin.tar.bz2' | head -n1)"
+    grep -oP '(?<=href=")/([a-zA-Z0-9\-/]+)/sde-external-([0-9\.\-]+)-lin.tar.bz2' | head -n1)"
 if [ ! -e "$1" ]; then
   mkdir -p "$1"
 fi
