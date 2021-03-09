7c10d7f8957d5be727bc3c42ed905c1a1dd51e18
Atharva Nimbalkar <atharvakn@gmail.com>
2021-03-09 11:05:04 +0530

    neon/bcax: Implmentation now uses BICQ. Fixes formatting.

--- simde/arm/neon/bcax.h
+++ simde/arm/neon/bcax.h
@@ -30,8 +30,7 @@
 #include "types.h"
 
 #include "eor.h"
-#include "mvn.h"
-#include "and.h"
+#include "bic.h"
 
 HEDLEY_DIAGNOSTIC_PUSH
 SIMDE_DISABLE_UNWANTED_DIAGNOSTICS
@@ -41,140 +40,112 @@ SIMDE_FUNCTION_ATTRIBUTES
 simde_uint8x16_t
 simde_vbcaxq_u8(simde_uint8x16_t a, simde_uint8x16_t b, simde_uint8x16_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_u8(a,b,c);
+    return vbcaxq_u8(a, b, c);
   #else
-    return simde_veorq_u8(a,simde_vandq_u8(b,simde_vmvnq_u8(c)));
+    return simde_veorq_u8(a, simde_vbicq_u8(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_u8
-  #define vbcaxq_u8(a,b,c) simde_vbcaxq_u8(a,b,c)
+  #define vbcaxq_u8(a, b, c) simde_vbcaxq_u8(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_uint16x8_t
 simde_vbcaxq_u16(simde_uint16x8_t a, simde_uint16x8_t b, simde_uint16x8_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_u16(a,b,c);
+    return vbcaxq_u16(a, b, c);
   #else
-    return simde_veorq_u16(a,simde_vandq_u16(b,simde_vmvnq_u16(c)));
+    return simde_veorq_u16(a, simde_vbicq_u16(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_u16
-  #define vbcaxq_u16(a,b,c) simde_vbcaxq_u16(a,b,c)
+  #define vbcaxq_u16(a, b, c) simde_vbcaxq_u16(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_uint32x4_t
 simde_vbcaxq_u32(simde_uint32x4_t a, simde_uint32x4_t b, simde_uint32x4_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_u32(a,b,c);
+    return vbcaxq_u32(a, b, c);
   #else
-    return simde_veorq_u32(a,simde_vandq_u32(b,simde_vmvnq_u32(c)));
+    return simde_veorq_u32(a, simde_vbicq_u32(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_u32
-  #define vbcaxq_u32(a,b,c) simde_vbcaxq_u32(a,b,c)
+  #define vbcaxq_u32(a, b, c) simde_vbcaxq_u32(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_uint64x2_t
 simde_vbcaxq_u64(simde_uint64x2_t a, simde_uint64x2_t b, simde_uint64x2_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_u64(a,b,c);
+    return vbcaxq_u64(a, b, c);
   #else
-    simde_uint64x2_private
-      nc_,
-      c_ = simde_uint64x2_to_private(c);
-
-    #if defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      nc_.values = ~c_.values;
-    #else
-      SIMDE_VECTORIZE
-      for (size_t i = 0 ; i < (sizeof(c_.values) / sizeof(c_.values[0])) ; i++) {
-        nc_.values[i] = ~(c_.values[i]);
-      }
-    #endif
-
-    simde_uint64x2_t nc = simde_uint64x2_from_private(nc_);
-    return simde_veorq_u64(a,simde_vandq_u64(b,nc));
+    return simde_veorq_u64(a, simde_vbicq_u64(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_u64
-  #define vbcaxq_u64(a,b,c) simde_vbcaxq_u64(a,b,c)
+  #define vbcaxq_u64(a, b, c) simde_vbcaxq_u64(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_int8x16_t
 simde_vbcaxq_s8(simde_int8x16_t a, simde_int8x16_t b, simde_int8x16_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_s8(a,b,c);
+    return vbcaxq_s8(a, b, c);
   #else
-    return simde_veorq_s8(a,simde_vandq_s8(b,simde_vmvnq_s8(c)));
+    return simde_veorq_s8(a, simde_vbicq_s8(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_s8
-  #define vbcaxq_s8(a,b,c) simde_vbcaxq_s8(a,b,c)
+  #define vbcaxq_s8(a, b, c) simde_vbcaxq_s8(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_int16x8_t
 simde_vbcaxq_s16(simde_int16x8_t a, simde_int16x8_t b, simde_int16x8_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_s16(a,b,c);
+    return vbcaxq_s16(a, b, c);
   #else
-    return simde_veorq_s16(a,simde_vandq_s16(b,simde_vmvnq_s16(c)));
+    return simde_veorq_s16(a,simde_vbicq_s16(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_s16
-  #define vbcaxq_s16(a,b,c) simde_vbcaxq_s16(a,b,c)
+  #define vbcaxq_s16(a, b, c) simde_vbcaxq_s16(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_int32x4_t
 simde_vbcaxq_s32(simde_int32x4_t a, simde_int32x4_t b, simde_int32x4_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_s32(a,b,c);
+    return vbcaxq_s32(a, b, c);
   #else
-    return simde_veorq_s32(a,simde_vandq_s32(b,simde_vmvnq_s32(c)));
+    return simde_veorq_s32(a, simde_vbicq_s32(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_s32
-  #define vbcaxq_s32(a,b,c) simde_vbcaxq_s32(a,b,c)
+  #define vbcaxq_s32(a, b, c) simde_vbcaxq_s32(a, b, c)
 #endif
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde_int64x2_t
 simde_vbcaxq_s64(simde_int64x2_t a, simde_int64x2_t b, simde_int64x2_t c) {
   #if defined(SIMDE_ARM_NEON_A64V8_NATIVE) && defined(__ARM_FEATURE_SHA3)
-    return vbcaxq_s64(a,b,c);
+    return vbcaxq_s64(a, b, c);
   #else
-    simde_int64x2_private
-      nc_,
-      c_ = simde_int64x2_to_private(c);
-
-    #if defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      nc_.values = ~c_.values;
-    #else
-      SIMDE_VECTORIZE
-      for (size_t i = 0 ; i < (sizeof(c_.values) / sizeof(c_.values[0])) ; i++) {
-        nc_.values[i] = ~(c_.values[i]);
-      }
-    #endif
-
-    simde_int64x2_t nc = simde_int64x2_from_private(nc_);
-    return simde_veorq_s64(a,simde_vandq_s64(b,nc));
+    return simde_veorq_s64(a, simde_vbicq_s64(b, c));
   #endif
 }
 #if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES) || (defined(SIMDE_ENABLE_NATIVE_ALIASES) && !defined(__ARM_FEATURE_SHA3))
   #undef vbcaxq_s64
-  #define vbcaxq_s64(a,b,c) simde_vbcaxq_s64(a,b,c)
+  #define vbcaxq_s64(a, b, c) simde_vbcaxq_s64(a, b, c)
 #endif
 
 SIMDE_END_DECLS_
