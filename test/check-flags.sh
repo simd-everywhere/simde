373e1e3590f06875e3270131174b4e36bb89471d
Evan Nemerson <evan@nemerson.com>
2021-04-25 20:16:28 -0400

    check-flags.sh: add lock around installing SDE
    I think this is what was causing the occasional failures about
    libunwind-dynamic.so not being found.

--- test/check-flags.sh
+++ test/check-flags.sh
@@ -4,8 +4,6 @@
 # run the tests if the extension is supported, but skip them if it
 # isn't.  Currently this only works on x86.
 
-echo "WD: $PWD" > ~/wd.txt
-
 if [ -z "${SDE_PATH}" ]; then
   SDE_PATH="/opt/intel/sde"
 fi
@@ -16,8 +14,15 @@ fi
 
 NATIVE_SUPPORT=1
 for FLAG in $CFLAGS; do
-  echo $FLAG | grep -qP '^\-m[0-9a-zA-V\.]+$' || continue
-  grep -qP " ${FLAG:2} " /proc/cpuinfo || NATIVE_SUPPORT=0
+  echo $FLAG | grep -qP '^\-m[0-9a-zA-Z\.]+$' || continue
+  case "${FLAG:2}" in
+    # sse3 doesn't show up in /proc/cpuinfo (?!?!)
+    "sse3")
+      ;;
+    *)
+      grep -qP " ${FLAG:2} " /proc/cpuinfo || NATIVE_SUPPORT=0
+      ;;
+  esac
 done
 
 COMMAND="$1"
@@ -39,9 +44,12 @@ elif [ $NATIVE_SUPPORT -eq 1 ]; then
   $@
   exit $?
 elif [ "$COMMAND" = "sde" ]; then
-  if [ ! -e "${SDE_PATH}/sde64" ]; then
-    "$(dirname "$0")"/download-sde.sh "${SDE_PATH}"
-  fi
+  (
+    flock 9
+    if [ ! -e "${SDE_PATH}/sde64" ]; then
+      "$(dirname "$0")"/download-sde.sh "${SDE_PATH}"
+    fi
+  ) 9>/tmp/sde-download.lock
   "${SDE_PATH}/sde64" -- $@
   exit $?
 else
