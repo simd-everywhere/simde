6b07d80e9e563da2576f01f30fa703eb9a52a8ea
kunwar maheep <kunwar.maheep@students.iiit.ac.in>
2021-05-22 18:33:12 +0530

    avx512/roundscale.h: fixed reinterpret cast bug

--- simde/x86/avx512/roundscale.h
+++ simde/x86/avx512/roundscale.h
@@ -15,13 +15,18 @@ SIMDE_BEGIN_DECLS_
   simde__m128
   simde_mm_roundscale_ps (simde__m128 a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE(imm8, 0, 255) {
-    simde__m128 temp, r, clear_sign;
+    simde__m128 r, clear_sign;
+    simde_float32 exponent1f, exponent2f;
     int32_t
       exponent1 = (127 + ((imm8 >> 4) & 15)) << 23,
       exponent2 = (127 - ((imm8 >> 4) & 15)) << 23;
 
-    SIMDE_CONSTIFY_16_(simde_mm_round_ps, temp, simde_mm_setzero_ps(), imm8 & 15, simde_mm_mul_ps(a, simde_mm_set1_ps(*HEDLEY_REINTERPRET_CAST(SIMDE_FLOAT32_TYPE*, &exponent1))));
-    r = simde_mm_mul_ps(simde_mm_set1_ps(*HEDLEY_REINTERPRET_CAST(SIMDE_FLOAT32_TYPE*, &exponent2)), temp);
+    simde_memcpy(&exponent1f, &exponent1, sizeof(exponent1f));
+    simde_memcpy(&exponent2f, &exponent2, sizeof(exponent2f));
+
+    r = simde_mm_mul_ps(a, simde_mm_set1_ps(exponent1f));
+    SIMDE_CONSTIFY_16_(simde_mm_round_ps, r, (HEDLEY_UNREACHABLE(), simde_mm_setzero_ps()), imm8 & 15, r);
+    r = simde_mm_mul_ps(r, simde_mm_set1_ps(exponent2f));
 
     clear_sign = simde_mm_andnot_ps(simde_mm_set1_ps(SIMDE_FLOAT32_C(-0.0)), r);
     r = simde_x_mm_select_ps(r, a, simde_mm_cmpeq_ps(clear_sign, simde_mm_set1_ps(SIMDE_MATH_INFINITY)));
