a48df192e5de53d124e4fbf80627906534f279b0
kunwar maheep <kunwar.maheep@students.iiit.ac.in>
2021-06-22 16:31:39 +0530

    moved implementation to a loop

--- simde/x86/avx512/ror.h
+++ simde/x86/avx512/ror.h
@@ -17,18 +17,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m128i
-  simde_mm_ror_epi32_internal_(simde__m128i a, simde__m128i result, int imm8)
+  simde_mm_ror_epi32 (simde__m128i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m128i retval;
+    simde__m128i_private
+      r_,
+      a_ = simde__m128i_to_private(a);
 
-    if(imm8 & 31)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 31) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u32) / sizeof(r_.u32[0])) ; i++) {
+          r_.u32[i] = (a_.u32[i] >> (imm8 & 31)) | (a_.u32[i] << (32 - (imm8 & 31)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m128i_from_private(r_);
   }
-  #define simde_mm_ror_epi32(a, imm8) simde_mm_ror_epi32_internal_(a, simde_mm_or_si128(simde_mm_srli_epi32(a, (imm8 & 31)), simde_mm_slli_epi32(a, (32 - (imm8 & 31)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm_ror_epi32
@@ -60,18 +68,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m256i
-  simde_mm256_ror_epi32_internal_(simde__m256i a, simde__m256i result, int imm8)
+  simde_mm256_ror_epi32 (simde__m256i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m256i retval;
+    simde__m256i_private
+      r_,
+      a_ = simde__m256i_to_private(a);
 
-    if(imm8 & 31)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 31) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u32) / sizeof(r_.u32[0])) ; i++) {
+          r_.u32[i] = (a_.u32[i] >> (imm8 & 31)) | (a_.u32[i] << (32 - (imm8 & 31)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m256i_from_private(r_);
   }
-  #define simde_mm256_ror_epi32(a, imm8) simde_mm256_ror_epi32_internal_(a, simde_mm256_or_si256(simde_mm256_srli_epi32(a, (imm8 & 31)), simde_mm256_slli_epi32(a, (32 - (imm8 & 31)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm256_ror_epi32
@@ -103,18 +119,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m512i
-  simde_mm512_ror_epi32_internal_(simde__m512i a, simde__m512i result, int imm8)
+  simde_mm512_ror_epi32 (simde__m512i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m512i retval;
+    simde__m512i_private
+      r_,
+      a_ = simde__m512i_to_private(a);
 
-    if(imm8 & 31)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 31) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u32) / sizeof(r_.u32[0])) ; i++) {
+          r_.u32[i] = (a_.u32[i] >> (imm8 & 31)) | (a_.u32[i] << (32 - (imm8 & 31)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m512i_from_private(r_);
   }
-  #define simde_mm512_ror_epi32(a, imm8) simde_mm512_ror_epi32_internal_(a, simde_mm512_or_epi32(simde_mm512_srli_epi32(a, (imm8 & 31)), simde_mm512_slli_epi32(a, (32 - (imm8 & 31)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES)
   #undef _mm512_ror_epi32
@@ -146,18 +170,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m128i
-  simde_mm_ror_epi64_internal_(simde__m128i a, simde__m128i result, int imm8)
+  simde_mm_ror_epi64 (simde__m128i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m128i retval;
+    simde__m128i_private
+      r_,
+      a_ = simde__m128i_to_private(a);
 
-    if(imm8 & 63)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 63) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u64) / sizeof(r_.u64[0])) ; i++) {
+          r_.u64[i] = (a_.u64[i] >> (imm8 & 63)) | (a_.u64[i] << (64 - (imm8 & 63)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m128i_from_private(r_);
   }
-  #define simde_mm_ror_epi64(a, imm8) simde_mm_ror_epi64_internal_(a, simde_mm_or_si128(simde_mm_srli_epi64(a, (imm8 & 63)), simde_mm_slli_epi64(a, (64 - (imm8 & 63)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm_ror_epi64
@@ -189,18 +221,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m256i
-  simde_mm256_ror_epi64_internal_(simde__m256i a, simde__m256i result, int imm8)
+  simde_mm256_ror_epi64 (simde__m256i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m256i retval;
+    simde__m256i_private
+      r_,
+      a_ = simde__m256i_to_private(a);
 
-    if(imm8 & 63)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 63) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u64) / sizeof(r_.u64[0])) ; i++) {
+          r_.u64[i] = (a_.u64[i] >> (imm8 & 63)) | (a_.u64[i] << (64 - (imm8 & 63)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m256i_from_private(r_);
   }
-  #define simde_mm256_ror_epi64(a, imm8) simde_mm256_ror_epi64_internal_(a, simde_mm256_or_si256(simde_mm256_srli_epi64(a, (imm8 & 63)), simde_mm256_slli_epi64(a, (64 - (imm8 & 63)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
   #undef _mm256_ror_epi64
@@ -232,18 +272,26 @@ SIMDE_BEGIN_DECLS_
 #else
   SIMDE_FUNCTION_ATTRIBUTES
   simde__m512i
-  simde_mm512_ror_epi64_internal_(simde__m512i a, simde__m512i result, int imm8)
+  simde_mm512_ror_epi64 (simde__m512i a, int imm8)
       SIMDE_REQUIRE_CONSTANT_RANGE (imm8, 0, 255) {
-    simde__m512i retval;
+    simde__m512i_private
+      r_,
+      a_ = simde__m512i_to_private(a);
 
-    if(imm8 & 63)
-      retval = result;
-    else
-      retval = a;
+    switch (imm8 & 63) {
+      case 0:
+        r_ = a_;
+        break;
+      default:
+        SIMDE_VECTORIZE
+        for (size_t i = 0 ; i < (sizeof(r_.u64) / sizeof(r_.u64[0])) ; i++) {
+          r_.u64[i] = (a_.u64[i] >> (imm8 & 63)) | (a_.u64[i] << (64 - (imm8 & 63)));
+        }
+        break;
+    }
 
-    return retval;
+    return simde__m512i_from_private(r_);
   }
-  #define simde_mm512_ror_epi64(a, imm8) simde_mm512_ror_epi64_internal_(a, simde_mm512_or_epi64(simde_mm512_srli_epi64(a, (imm8 & 63)), simde_mm512_slli_epi64(a, (64 - (imm8 & 63)))), imm8)
 #endif
 #if defined(SIMDE_X86_AVX512F_ENABLE_NATIVE_ALIASES)
   #undef _mm512_ror_epi64
