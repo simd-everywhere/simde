8e208a7199463a043795e8d3facb29cc9671c707
kunwar maheep <kunwar.maheep@students.iiit.ac.in>
2021-07-09 02:22:33 +0530

    improve implementation

--- simde/x86/avx512/popcnt.h
+++ simde/x86/avx512/popcnt.h
@@ -9,6 +9,67 @@ SIMDE_BEGIN_DECLS_
 
 SIMDE_FUNCTION_ATTRIBUTES
 simde__m128i
+simde_mm_popcnt_epi8 (simde__m128i a) {
+  #if defined(SIMDE_X86_AVX512BITALG_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
+    return _mm_popcnt_epi8(a);
+  #else
+    simde__m128i_private
+      r_,
+      a_ = simde__m128i_to_private(a);
+
+    #if defined(SIMDE_ARM_NEON_A32V7_NATIVE)
+      r_.neon_i8 = vcntq_s8(a_.neon_i8);
+    #elif defined(SIMDE_WASM_SIMD128_NATIVE)
+      r_.wasm_v128 = wasm_i8x16_popcnt(a_.wasm_v128);
+    #elif defined(SIMDE_X86_SSSE3_NATIVE)
+      HEDLEY_STATIC_CAST(void, a_);
+      __m128i tmp0 = _mm_set1_epi8(0x0f);
+      __m128i tmp1 = _mm_andnot_si128(tmp0, a);
+      __m128i y = _mm_and_si128(tmp0, a);
+      tmp0 = _mm_set_epi8(4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0);
+      tmp1 = _mm_srli_epi16(tmp1, 4);
+      y = _mm_shuffle_epi8(tmp0, y);
+      tmp1 = _mm_shuffle_epi8(tmp0, tmp1);
+      r_ = simde__m128i_to_private(_mm_add_epi8(y, tmp1));
+    #elif defined(SIMDE_X86_SSE2_NATIVE)
+      HEDLEY_STATIC_CAST(void, a_);
+      __m128i tmp0 = _mm_and_si128(_mm_srli_epi16(a, 1), _mm_set1_epi8(0x55));
+      __m128i tmp1 = _mm_sub_epi8(a, tmp0);
+      tmp0 = tmp1;
+      tmp1 = _mm_and_si128(tmp1, _mm_set1_epi8(0x33));
+      tmp0 = _mm_and_si128(_mm_srli_epi16(tmp0, 2), _mm_set1_epi8(0x33));
+      tmp1 = _mm_add_epi8(tmp1, tmp0);
+      tmp0 = _mm_srli_epi16(tmp1, 4);
+      tmp1 = _mm_add_epi8(tmp1, tmp0);
+      r_ = simde__m128i_to_private(_mm_and_si128(tmp1, _mm_set1_epi8(0x0f)));
+    #elif defined(SIMDE_POWER_ALTIVEC_P8_NATIVE)
+      r_.altivec_i8 = HEDLEY_REINTERPRET_CAST(SIMDE_POWER_ALTIVEC_VECTOR(signed char), vec_popcnt(HEDLEY_REINTERPRET_CAST(SIMDE_POWER_ALTIVEC_VECTOR(unsigned char), a_.altivec_i8)));
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_SCALAR)
+      a_.u8 -= ((a_.u8 >> 1) & 0x55);
+      a_.u8  = ((a_.u8 & 0x33) + ((a_.u8 >> 2) & 0x33));
+      a_.u8  = (a_.u8 + (a_.u8 >> 4)) & 15;
+      r_.u8  = a_.u8 >> ((sizeof(uint8_t) - 1) * CHAR_BIT);
+    #else
+      SIMDE_VECTORIZE
+      for (size_t i = 0 ; i < (sizeof(r_.u8) / sizeof(r_.u8[0])) ; i++) {
+        uint8_t v = HEDLEY_STATIC_CAST(uint8_t, a_.u8[i]);
+        v -= ((v >> 1) & 0x55);
+        v  = (v & 0x33) + ((v >> 2) & 0x33);
+        v  = (v + (v >> 4)) & 0xf;
+        r_.u8[i] = v >> (sizeof(uint8_t) - 1) * CHAR_BIT;
+      }
+    #endif
+
+    return simde__m128i_from_private(r_);
+  #endif
+}
+#if defined(SIMDE_X86_AVX512BITALG_ENABLE_NATIVE_ALIASES) && defined(SIMDE_X86_AVX512VL_ENABLE_NATIVE_ALIASES)
+  #undef _mm_popcnt_epi8
+  #define _mm_popcnt_epi8(a) simde_mm_popcnt_epi8(a)
+#endif
+
+SIMDE_FUNCTION_ATTRIBUTES
+simde__m128i
 simde_mm_popcnt_epi16 (simde__m128i a) {
   #if defined(SIMDE_X86_AVX512BITALG_NATIVE) && defined(SIMDE_X86_AVX512VL_NATIVE)
     return _mm_popcnt_epi16(a);
@@ -17,21 +78,35 @@ simde_mm_popcnt_epi16 (simde__m128i a) {
       r_,
       a_ = simde__m128i_to_private(a);
 
-    #if defined(SIMDE_VECTOR_SUBSCRIPT_SCALAR)
-      a_.u16 -= ((a_.u16 >> 1) & 21845);
-      a_.u16 = ((a_.u16 & 13107) + ((a_.u16 >> 2) & 13107));
-      a_.u16 = (a_.u16 + (a_.u16 >> 4)) & 3855;
-      r_.u16 = (a_.u16 * 257) >> ((sizeof(uint16_t) - 1) * CHAR_BIT);
+    #if defined(SIMDE_ARM_NEON_A32V7_NATIVE)
+      r_.neon_i16 = vpaddlq_s8(vcntq_s8(a_.neon_i8));
+    #elif defined(SIMDE_WASM_SIMD128_NATIVE)
+      r_.wasm_v128 = wasm_i16x8_extadd_pairwise_i8x16(wasm_i8x16_popcnt(a_.wasm_v128));
     #elif defined(SIMDE_POWER_ALTIVEC_P8_NATIVE)
       r_.altivec_u16 = HEDLEY_REINTERPRET_CAST(SIMDE_POWER_ALTIVEC_VECTOR(unsigned short), vec_popcnt(HEDLEY_REINTERPRET_CAST(SIMDE_POWER_ALTIVEC_VECTOR(unsigned short), a_.altivec_u16)));
+    #elif defined(SIMDE_X86_XOP_NATIVE)
+      HEDLEY_STATIC_CAST(void, a_);
+      __m128i tmp0 = _mm_set1_epi8(0x0f);
+      __m128i tmp1 = _mm_andnot_si128(tmp0, a);
+      __m128i y = _mm_and_si128(tmp0, a);
+      tmp0 = _mm_set_epi8(4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0);
+      tmp1 = _mm_srli_epi16(tmp1, 4);
+      y = _mm_shuffle_epi8(tmp0, y);
+      tmp1 = _mm_shuffle_epi8(tmp0, tmp1);
+      r_ = simde__m128i_to_private(_mm_haddw_epi8(_mm_add_epi8(y, tmp1)));
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_SCALAR)
+      a_.u16 -= ((a_.u16 >> 1) & 0x5555);
+      a_.u16  = ((a_.u16 & 0x3333) + ((a_.u16 >> 2) & 0x3333));
+      a_.u16  = (a_.u16 + (a_.u16 >> 4)) & 0x0f0f;
+      r_.u16  = (a_.u16 * 0x0101) >> ((sizeof(uint16_t) - 1) * CHAR_BIT);
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.u16) / sizeof(r_.u16[0])) ; i++) {
         uint16_t v = HEDLEY_STATIC_CAST(uint16_t, a_.u16[i]);
-        v -= ((v >> 1) & 21845);
-        v = ((v & 13107) + ((v >> 2) & 13107));
-        v = (v + (v >> 4)) & 3855;
-        r_.u16[i] = HEDLEY_STATIC_CAST(uint16_t, (v * 257)) >> ((sizeof(uint16_t) - 1) * CHAR_BIT);
+        v -= ((v >> 1) & 0x5555);
+        v  = ((v & 0x3333) + ((v >> 2) & 0x3333));
+        v  = (v + (v >> 4)) & 0x0f0f;
+        r_.u16[i] = HEDLEY_STATIC_CAST(uint16_t, (v * 0x0101)) >> ((sizeof(uint16_t) - 1) * CHAR_BIT);
       }
     #endif
 
--- test/x86/avx512/popcnt.c
+++ test/x86/avx512/popcnt.c
@@ -4,6 +4,67 @@
 #include <simde/x86/avx512/popcnt.h>
 
 static int
+test_simde_mm_popcnt_epi8 (SIMDE_MUNIT_TEST_ARGS) {
+#if 1
+  static const struct {
+    const int8_t a[16];
+    const int8_t r[16];
+  } test_vec[] = {
+    { { -INT8_C(  95), -INT8_C( 112),  INT8_C(  35),  INT8_C(  57),  INT8_C( 117), -INT8_C(  49),  INT8_C(  91), -INT8_C( 116),
+         INT8_C(  48), -INT8_C(  74), -INT8_C(  33),  INT8_C(  26),  INT8_C(  20), -INT8_C( 110), -INT8_C(  77),  INT8_C( 125) },
+      {  INT8_C(   3),  INT8_C(   2),  INT8_C(   3),  INT8_C(   4),  INT8_C(   5),  INT8_C(   6),  INT8_C(   5),  INT8_C(   3),
+         INT8_C(   2),  INT8_C(   5),  INT8_C(   7),  INT8_C(   3),  INT8_C(   2),  INT8_C(   3),  INT8_C(   5),  INT8_C(   6) } },
+    { {  INT8_C(   4),  INT8_C(   1),  INT8_C(  71), -INT8_C(  23), -INT8_C(  44),  INT8_C(   8), -INT8_C(   5), -INT8_C(  69),
+         INT8_C(  21), -INT8_C(   3),  INT8_C(  44),  INT8_C(  97),  INT8_C(  81), -INT8_C(  14),  INT8_C( 125), -INT8_C(  13) },
+      {  INT8_C(   1),  INT8_C(   1),  INT8_C(   4),  INT8_C(   5),  INT8_C(   4),  INT8_C(   1),  INT8_C(   7),  INT8_C(   6),
+         INT8_C(   3),  INT8_C(   7),  INT8_C(   3),  INT8_C(   3),  INT8_C(   3),  INT8_C(   5),  INT8_C(   6),  INT8_C(   6) } },
+    { { -INT8_C( 125), -INT8_C(  96),  INT8_C(  44), -INT8_C(   8),  INT8_C( 111), -INT8_C( 121), -INT8_C( 124), -INT8_C(  97),
+         INT8_C(  62),  INT8_C(  99), -INT8_C(  70),  INT8_C(  82), -INT8_C(  11),  INT8_C( 109), -INT8_C(  48), -INT8_C(   7) },
+      {  INT8_C(   3),  INT8_C(   2),  INT8_C(   3),  INT8_C(   5),  INT8_C(   6),  INT8_C(   4),  INT8_C(   2),  INT8_C(   6),
+         INT8_C(   5),  INT8_C(   4),  INT8_C(   5),  INT8_C(   3),  INT8_C(   6),  INT8_C(   5),  INT8_C(   3),  INT8_C(   6) } },
+    { {  INT8_C( 110),  INT8_C(  23), -INT8_C(  30),  INT8_C(  67),  INT8_C(  31), -INT8_C(  35), -INT8_C(   2),  INT8_C(  53),
+        -INT8_C(  37),  INT8_C(  42), -INT8_C( 106),  INT8_C(  44),  INT8_C(  29),  INT8_C(  19),  INT8_C(  31), -INT8_C(  96) },
+      {  INT8_C(   5),  INT8_C(   4),  INT8_C(   4),  INT8_C(   3),  INT8_C(   5),  INT8_C(   6),  INT8_C(   7),  INT8_C(   4),
+         INT8_C(   6),  INT8_C(   3),  INT8_C(   4),  INT8_C(   3),  INT8_C(   4),  INT8_C(   3),  INT8_C(   5),  INT8_C(   2) } },
+    { { -INT8_C(  76),  INT8_C(  75), -INT8_C( 104),  INT8_C(  35), -INT8_C(  45),  INT8_C(  28), -INT8_C(  61),  INT8_C(  17),
+             INT8_MIN,  INT8_C( 125),  INT8_C(  99),  INT8_C( 117), -INT8_C(  22),  INT8_C(  51),  INT8_C( 111),  INT8_C(  88) },
+      {  INT8_C(   4),  INT8_C(   4),  INT8_C(   3),  INT8_C(   3),  INT8_C(   5),  INT8_C(   3),  INT8_C(   4),  INT8_C(   2),
+         INT8_C(   1),  INT8_C(   6),  INT8_C(   4),  INT8_C(   5),  INT8_C(   5),  INT8_C(   4),  INT8_C(   6),  INT8_C(   3) } },
+    { {  INT8_C(  75),  INT8_C(  81), -INT8_C( 101),  INT8_C( 106),  INT8_C(  47), -INT8_C( 102), -INT8_C(  97),  INT8_C(  10),
+        -INT8_C(  60),  INT8_C(  53),  INT8_C(  54), -INT8_C(  31),  INT8_C(  73),  INT8_C(  86), -INT8_C( 127), -INT8_C(   3) },
+      {  INT8_C(   4),  INT8_C(   3),  INT8_C(   5),  INT8_C(   4),  INT8_C(   5),  INT8_C(   4),  INT8_C(   6),  INT8_C(   2),
+         INT8_C(   3),  INT8_C(   4),  INT8_C(   4),  INT8_C(   4),  INT8_C(   3),  INT8_C(   4),  INT8_C(   2),  INT8_C(   7) } },
+    { { -INT8_C(  95),  INT8_C(  25),  INT8_C(  32),  INT8_C( 116),  INT8_C(  54), -INT8_C(  29), -INT8_C( 123), -INT8_C(  74),
+         INT8_C(  96), -INT8_C(  23),  INT8_C(  43),  INT8_C(  74),  INT8_C(  28), -INT8_C( 102), -INT8_C(  93),  INT8_C( 103) },
+      {  INT8_C(   3),  INT8_C(   3),  INT8_C(   1),  INT8_C(   4),  INT8_C(   4),  INT8_C(   5),  INT8_C(   3),  INT8_C(   5),
+         INT8_C(   2),  INT8_C(   5),  INT8_C(   4),  INT8_C(   3),  INT8_C(   3),  INT8_C(   4),  INT8_C(   4),  INT8_C(   5) } },
+    { { -INT8_C(  20),  INT8_C(  62), -INT8_C(  46),  INT8_C(  27), -INT8_C(  40),  INT8_C( 113),  INT8_C(  37), -INT8_C(  99),
+        -INT8_C(  89),  INT8_C(  91),  INT8_C( 126), -INT8_C(  16), -INT8_C(  79),  INT8_C(   0), -INT8_C(  19),  INT8_C(  83) },
+      {  INT8_C(   5),  INT8_C(   5),  INT8_C(   4),  INT8_C(   4),  INT8_C(   4),  INT8_C(   4),  INT8_C(   3),  INT8_C(   5),
+         INT8_C(   5),  INT8_C(   5),  INT8_C(   6),  INT8_C(   4),  INT8_C(   4),  INT8_C(   0),  INT8_C(   6),  INT8_C(   4) } },
+  };
+
+  for (size_t i = 0 ; i < (sizeof(test_vec) / sizeof(test_vec[0])) ; i++) {
+    simde__m128i a = simde_x_mm_loadu_epi8(test_vec[i].a);
+    simde__m128i r = simde_mm_popcnt_epi8(a);
+    simde_test_x86_assert_equal_i8x16(r, simde_x_mm_loadu_epi8(test_vec[i].r));
+  }
+
+  return 0;
+#else
+  fputc('\n', stdout);
+  for (int i = 0 ; i < 8 ; i++) {
+    simde__m128i a = simde_test_x86_random_i8x16();
+    simde__m128i r = simde_mm_popcnt_epi8(a);
+
+    simde_test_x86_write_i8x16(2, a, SIMDE_TEST_VEC_POS_FIRST);
+    simde_test_x86_write_i8x16(2, r, SIMDE_TEST_VEC_POS_LAST);
+  }
+  return 1;
+#endif
+}
+
+static int
 test_simde_mm_popcnt_epi16 (SIMDE_MUNIT_TEST_ARGS) {
 #if 1
   static const struct {
@@ -49,6 +110,7 @@ test_simde_mm_popcnt_epi16 (SIMDE_MUNIT_TEST_ARGS) {
 }
 
 SIMDE_TEST_FUNC_LIST_BEGIN
+  SIMDE_TEST_FUNC_LIST_ENTRY(mm_popcnt_epi8)
   SIMDE_TEST_FUNC_LIST_ENTRY(mm_popcnt_epi16)
 SIMDE_TEST_FUNC_LIST_END
 
