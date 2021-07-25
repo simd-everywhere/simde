91e3edc8df6f45f40102d8d64234fd7059e3674c
kunwar maheep <kunwar.maheep@students.iiit.ac.in>
2021-07-25 17:01:45 +0530

    add subscript scalar implementations

--- simde/x86/avx512/bitshuffle.h
+++ simde/x86/avx512/bitshuffle.h
@@ -18,12 +18,25 @@ simde_mm_bitshuffle_epi64_mask (simde__m128i b, simde__m128i c) {
       c_ = simde__m128i_to_private(c);
     simde__mmask16 r = 0;
 
-    for (size_t i = 0 ; i < (sizeof(c_.m64_private) / sizeof(c_.m64_private[0])) ; i++) {
-      SIMDE_VECTORIZE_REDUCTION(|:r)
-      for (size_t j = 0 ; j < (sizeof(c_.m64_private[i].u8) / sizeof(c_.m64_private[i].u8[0])) ; j++) {
-        r |= (((b_.u64[i] >> (c_.m64_private[i].u8[j] & 0x3f)) & 1) << ((i * 8) + j));
+    #if defined(SIMDE_VECTOR_SUBSCRIPT_SCALAR)
+      __typeof__(b_.u64) rv = { 0, 0 };
+      __typeof__(b_.u64) lshift = { 0, 8 };
+
+      for (int8_t i = 0 ; i < 8 ; i++) {
+        __typeof__(b_.u64) ct = (HEDLEY_REINTERPRET_CAST(__typeof__(ct), c_.u8) >> (i * 8)) & 63;
+        rv |= ((b_.u64 >> ct) & 1) << lshift;
+        lshift += 1;
       }
-    }
+
+      r = HEDLEY_STATIC_CAST(simde__mmask16, rv[0]) | HEDLEY_STATIC_CAST(simde__mmask16, rv[1]);
+    #else
+      for (size_t i = 0 ; i < (sizeof(c_.m64_private) / sizeof(c_.m64_private[0])) ; i++) {
+        SIMDE_VECTORIZE_REDUCTION(|:r)
+        for (size_t j = 0 ; j < (sizeof(c_.m64_private[i].u8) / sizeof(c_.m64_private[i].u8[0])) ; j++) {
+          r |= (((b_.u64[i] >> (c_.m64_private[i].u8[j]) & 63) & 1) << ((i * 8) + j));
+        }
+      }
+    #endif
 
     return r;
   #endif
