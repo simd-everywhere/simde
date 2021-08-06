082a87572d40b486e689f76cf644a6f5651e5fd4
Evan Nemerson <evan@nemerson.com>
2021-08-05 22:54:10 -0400

    download-iig: tweak script to fix download location
    The version number we were looking at before changed to 3.6.1, but
    elsewhere in he page is still using 3.6.0.

--- test/download-iig.sh
+++ test/download-iig.sh
@@ -7,6 +7,6 @@ else
 fi
 
 if [ ! -e "${OUTFILE}" ]; then
-  IIG_VERSION="$(curl -s https://software.intel.com/sites/landingpage/IntrinsicsGuide/ | grep -Po '(?<=intrinsicsguide.min.js\?)([0-9\.]+)')"
+  IIG_VERSION="$(curl -s https://software.intel.com/sites/landingpage/IntrinsicsGuide/ | grep -Po '(?<=style.min.css\?)([0-9\.]+)')"
   curl -s "https://software.intel.com/sites/landingpage/IntrinsicsGuide/files/data-${IIG_VERSION}.xml" > "${OUTFILE}";
 fi
