2fdc9a5e5f078cb11107bdd5c79b6a5fa6d8bf43
Evan Nemerson <evan@nemerson.com>
2021-10-15 09:14:26 -0400

    tests: update download-iig.sh to account for Intel changes

--- test/download-iig.sh
+++ test/download-iig.sh
@@ -7,6 +7,6 @@ else
 fi
 
 if [ ! -e "${OUTFILE}" ]; then
-  IIG_VERSION="$(curl -s https://software.intel.com/sites/landingpage/IntrinsicsGuide/ | grep -Po '(?<=style.min.css\?)([0-9\.]+)')"
-  curl -s "https://software.intel.com/sites/landingpage/IntrinsicsGuide/files/data-${IIG_VERSION}.xml" > "${OUTFILE}";
+  IIG_VERSION="$(curl -s https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html | grep -Po '(?<=style.min.css\?)([0-9.\-]+)')"
+  curl -s "https://www.intel.com/content/dam/develop/public/us/en/include/intrinsics-guide/data-${IIG_VERSION}.xml" > "${OUTFILE}";
 fi
