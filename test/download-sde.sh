55fc0e2c20a677b8522183561f3acf4b0011ebf3
Evan Nemerson <evan@nemerson.com>
2022-04-22 08:25:28 -0400

    sde: don't print URL in download-sde script.

--- test/download-sde.sh
+++ test/download-sde.sh
@@ -2,7 +2,6 @@
 
 URL="$(curl -sL 'https://www.intel.com/content/www/us/en/download/684897/intel-software-development-emulator.html' | \
     grep -oP '(?<=data\-direct\-path=")(https://[^\"]+)/sde-external-([0-9\.\-]+)-lin.tar.xz' | head -n1)"
-echo "${URL}"
 if [ ! -e "$1" ]; then
   mkdir -p "$1"
 fi
