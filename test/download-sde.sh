24338a285cb355777a94008a2cec2e9c2471a15c
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-01-04 11:28:35 +0100

    update SDE download link

--- test/download-sde.sh
+++ test/download-sde.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 URL="$(curl -sL 'https://www.intel.com/content/www/us/en/download/684897/intel-software-development-emulator.html' | \
-    grep -oP '(?<=data\-direct\-path=")(https://[^\"]+)/sde-external-([0-9\.\-]+)-lin.tar.xz' | head -n1)"
+    grep -oP '(?<=data\-href=")(https://[^\"]+)/sde-external-([0-9\.\-]+)-lin.tar.xz' | head -n1)"
 if [ ! -e "$1" ]; then
   mkdir -p "$1"
 fi
