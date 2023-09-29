2edce07639b7e7fd6d7ac4e1def9312bb78f730d
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-09-29 13:21:00 +0200

    some new functions are aarch64 only

--- simde/arm/neon/st1_x4.h
+++ simde/arm/neon/st1_x4.h
@@ -56,7 +56,7 @@ simde_vst1_f32_x4(simde_float32 ptr[HEDLEY_ARRAY_PARAM(8)], simde_float32x2x4_t 
 SIMDE_FUNCTION_ATTRIBUTES
 void
 simde_vst1_f64_x4(simde_float64 ptr[HEDLEY_ARRAY_PARAM(4)], simde_float64x1x4_t val) {
-  #if defined(SIMDE_ARM_NEON_A32V7_NATIVE)
+  #if defined(SIMDE_ARM_NEON_A64V8_NATIVE)
     vst1_f64_x4(ptr, val);
   #else
     simde_vst1_f64(ptr, val.val[0]);
@@ -65,7 +65,7 @@ simde_vst1_f64_x4(simde_float64 ptr[HEDLEY_ARRAY_PARAM(4)], simde_float64x1x4_t 
     simde_vst1_f64(ptr+3, val.val[3]);
   #endif
 }
-#if defined(SIMDE_ARM_NEON_A32V7_ENABLE_NATIVE_ALIASES)
+#if defined(SIMDE_ARM_NEON_A64V8_ENABLE_NATIVE_ALIASES)
   #undef vst1_f64_x4
   #define vst1_f64_x4(a, b) simde_vst1_f64_x4((a), (b))
 #endif
