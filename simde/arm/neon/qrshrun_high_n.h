ae7632b9cc40fd7e04c0d5d3acaba288d89e939d
Eric Yi-Yen Chung <eric681@andestech.com>
2023-10-19 15:31:37 +0800

    [Fix] Add copyright

--- simde/arm/neon/qrshrun_high_n.h
+++ simde/arm/neon/qrshrun_high_n.h
@@ -21,7 +21,7 @@
  * SOFTWARE.
  *
  * Copyright:
- *   2021      Zhi An Ng <zhin@google.com> (Copyright owned by Google, LLC)
+ *   2023      Yi-Yen Chung <eric681@andestech.com> (Copyright owned by Andes Technology)
  */
 
 #if !defined(SIMDE_ARM_NEON_QRSHRUN_HIGH_N_H)
