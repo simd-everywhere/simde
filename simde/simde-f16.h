600050dbd79edaca29cc9649ddb9502041111323
gxw <guxiwei-hf@loongson.cn>
2024-09-20 14:28:29 +0800

    loongarch float16: use a portable version to avoid compilation errors

--- simde/simde-f16.h
+++ simde/simde-f16.h
@@ -72,6 +72,7 @@ SIMDE_BEGIN_DECLS_
     !(defined(HEDLEY_MSVC_VERSION) && defined(__clang__)) && \
     !(defined(SIMDE_ARCH_MIPS) && defined(__clang__)) && \
     !(defined(SIMDE_ARCH_ZARCH) && defined(__clang__)) && \
+    !(defined(SIMDE_ARCH_LOONGARCH) && defined(__clang__)) && \
     !(defined(__clang__) && defined(SIMDE_ARCH_RISCV64)) && ( \
       defined(SIMDE_X86_AVX512FP16_NATIVE) || \
       (defined(SIMDE_ARCH_X86_SSE2) && HEDLEY_GCC_VERSION_CHECK(12,0,0)) || \
