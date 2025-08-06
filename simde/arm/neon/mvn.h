966b34e2237fa31e1cd58869e494baa61add9bdb
Michael R. Crusoe <michael.crusoe@gmail.com>
2025-08-06 18:09:54 +0200

    prefer NATIVE implementations over SIMDE_VECTOR_SUBSCRIPT_OPS

--- simde/arm/neon/mvn.h
+++ simde/arm/neon/mvn.h
@@ -55,10 +55,10 @@ simde_vmvnq_s8(simde_int8x16_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi8(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_i8m1(a_.sv128, 16);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -92,10 +92,10 @@ simde_vmvnq_s16(simde_int16x8_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi16(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_i16m1(a_.sv128, 8);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -129,10 +129,10 @@ simde_vmvnq_s32(simde_int32x4_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi32(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_i32m1(a_.sv128, 4);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -166,10 +166,10 @@ simde_vmvnq_u8(simde_uint8x16_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi8(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_u8m1(a_.sv128, 16);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -203,10 +203,10 @@ simde_vmvnq_u16(simde_uint16x8_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi16(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_u16m1(a_.sv128, 8);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -240,10 +240,10 @@ simde_vmvnq_u32(simde_uint32x4_t a) {
       r_.m128i = _mm_andnot_si128(a_.m128i, _mm_cmpeq_epi32(a_.m128i, a_.m128i));
     #elif defined(SIMDE_WASM_SIMD128_NATIVE)
       r_.v128 = wasm_v128_not(a_.v128);
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv128 = __riscv_vnot_v_u32m1(a_.sv128, 4);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -271,10 +271,10 @@ simde_vmvn_s8(simde_int8x8_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi8(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_i8m1(a_.sv64, 8);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -302,10 +302,10 @@ simde_vmvn_s16(simde_int16x4_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi16(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_i16m1(a_.sv64, 4);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -333,10 +333,10 @@ simde_vmvn_s32(simde_int32x2_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi32(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_i32m1(a_.sv64, 2);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -364,10 +364,10 @@ simde_vmvn_u8(simde_uint8x8_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi8(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_u8m1(a_.sv64, 8);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -395,10 +395,10 @@ simde_vmvn_u16(simde_uint16x4_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi16(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_u16m1(a_.sv64, 4);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
@@ -426,10 +426,10 @@ simde_vmvn_u32(simde_uint32x2_t a) {
 
     #if defined(SIMDE_X86_MMX_NATIVE)
       r_.m64 = _mm_andnot_si64(a_.m64, _mm_cmpeq_pi32(a_.m64, a_.m64));
-    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
-      r_.values = ~a_.values;
     #elif defined(SIMDE_RISCV_V_NATIVE)
       r_.sv64 = __riscv_vnot_v_u32m1(a_.sv64, 2);
+    #elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+      r_.values = ~a_.values;
     #else
       SIMDE_VECTORIZE
       for (size_t i = 0 ; i < (sizeof(r_.values) / sizeof(r_.values[0])) ; i++) {
