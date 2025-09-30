48d85f683fa0406dc3f59a1a7692cfa31e44af72
Thomas Magnusson <ethomag@users.noreply.github.com>
2025-09-30 14:40:21 +0200

    Applied mr-c's patch

--- simde/x86/avx.h
+++ simde/x86/avx.h
@@ -2179,10 +2179,8 @@ simde_mm256_castps128_ps256 (simde__m128 a) {
   #else
     simde__m256_private r_;
     simde__m128_private a_ = simde__m128_to_private(a);
-    #if defined(__CLANG) || defined(__GNUC__)
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-    #endif
+    HEDLEY_DIAGNOSTIC_PUSH
+    SIMDE_DIAGNOSTIC_DISABLE_MAYBE_UNINITIAZILED_
     r_.m128_private[0] = a_;
 
     return simde__m256_from_private(r_);
