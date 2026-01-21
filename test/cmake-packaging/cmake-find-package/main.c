272fcc2c0d275adcf1ee7b976aa5c970d4917279
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-01-21 16:09:00 +0100

    test: add cmake packaging unit tests

---
+++ test/cmake-packaging/cmake-find-package/main.c
@@ -0,0 +1,9 @@
+#include <simde/x86/sse.h>
+#include <stdio.h>
+
+int main(void) {
+  simde__m128 a = simde_mm_set_ps(1.0f, 2.0f, 3.0f, 4.0f);
+  (void)a;
+  printf("SIMDe cmake install test works!\n");
+  return 0;
+}
