f57b6e2bcda8b9faf70f8006679ce6274b760887
Felix Sternberg <felix@sternberg.cx>
2026-04-22 15:28:10 +0200

    x86/avx512: fix simde_mm512_mask_extracti64x2_epi64 and simde_mm512_maskz_extracti64x2_epi64 to use simde_mm_mask_mov_epi64 and simde_mm_maskz_mov_epi64

--- simde/x86/avx512/extract.h
+++ simde/x86/avx512/extract.h
@@ -244,7 +244,7 @@ simde_mm512_extracti64x2_epi64 (simde__m512i a, int imm8)
 #if defined(SIMDE_X86_AVX512DQ_NATIVE) && (!defined(HEDLEY_GCC_VERSION) || HEDLEY_GCC_VERSION_CHECK(7,0,0)) && !defined(SIMDE_BUG_CLANG_REV_299346)
 #define simde_mm512_mask_extracti64x2_epi64(src, k, a, imm8) _mm512_mask_extracti64x2_epi64(src, k, a, imm8)
 #else
-#define simde_mm512_mask_extracti64x2_epi64(src, k, a, imm8) simde_mm256_mask_mov_epi64((src), (k), simde_mm512_extracti64x2_epi64((a), (imm8)))
+#define simde_mm512_mask_extracti64x2_epi64(src, k, a, imm8) simde_mm_mask_mov_epi64((src), (k), simde_mm512_extracti64x2_epi64((a), (imm8)))
 #endif
 #if defined(SIMDE_X86_AVX512DQ_ENABLE_NATIVE_ALIASES)
 #undef _mm512_mask_extracti64x2_epi64
@@ -254,7 +254,7 @@ simde_mm512_extracti64x2_epi64 (simde__m512i a, int imm8)
 #if defined(SIMDE_X86_AVX512DQ_NATIVE) && (!defined(HEDLEY_GCC_VERSION) || HEDLEY_GCC_VERSION_CHECK(7,0,0)) && !defined(SIMDE_BUG_CLANG_REV_299346)
 #define simde_mm512_maskz_extracti64x2_epi64(k, a, imm8) _mm512_maskz_extracti64x2_epi64(k, a, imm8)
 #else
-#define simde_mm512_maskz_extracti64x2_epi64(k, a, imm8) simde_mm256_maskz_mov_epi64((k), simde_mm512_extracti64x2_epi64((a), (imm8)))
+#define simde_mm512_maskz_extracti64x2_epi64(k, a, imm8) simde_mm_maskz_mov_epi64((k), simde_mm512_extracti64x2_epi64((a), (imm8)))
 #endif
 #if defined(SIMDE_X86_AVX512DQ_ENABLE_NATIVE_ALIASES)
 #undef _mm512_maskz_extracti64x2_epi64
